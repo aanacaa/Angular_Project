7b772c9a8e36c200fb7153cd9146605b60f656d7
Ana Andrade <anacrln.andrade@gmail.com>
2020-08-07 09:21:04 -0300

    Leitura json
    tentativa de leitura da tabela

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,23 +1,35 @@
+import { MatTable } from '@angular/material/table';
+import { ProductRead2DataSource } from './../product-read2/product-read2-datasource';
 import { ProductService } from './../product.service';
 import { Product } from './../product.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-product-read',
   templateUrl: './product-read.component.html',
   styleUrls: ['./product-read.component.css']
 })
-export class ProductReadComponent implements OnInit {
+export class ProductReadComponent implements OnInit, AfterViewInit {
   products: Product[]
-
+  @ViewChild(MatTable) table: MatTable<Product>;
+  //dataSource: ProductService;
   constructor(private ProductService: ProductService) { }
 
   ngOnInit(): void {
     this.ProductService.read().subscribe(products => {
       this.products = products
+      this.table.dataSource = this.products;
       console.log(products)
     })
   }
+    /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+    displayedColumns = ['id', 'name', 'price'];
+  
+  
+    ngAfterViewInit() {
+      //this.table.dataSource = this.products;
 
+    }
+    
 
 }
